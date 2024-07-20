a9d0480519d06cb1832c2f4593edf5a2f2e0c69a
Seifkhaled1010 <seifkhaled1917@gmail.com>
2024-07-20 04:51:31 +0300

    Add project files.

---
+++ CRUD1/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using CRUD1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var records = _dbContext.Set<Product>().ToList();
+            return Ok(records);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            var record = _dbContext.Set<Product>().Find(id);
+            return record == null ? NotFound() : Ok(record);
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult<int> CreateProduct(Product product)
+        {
+            product.Id = 0;
+            _dbContext.Set<Product>().Add(product);
+            _dbContext.SaveChanges();
+            return Ok(product.Id);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public ActionResult UpdateProduct(Product product)
+        {
+            var existingProduct = _dbContext.Set<Product>().Find(product.Id);
+            existingProduct.Name = product.Name;
+            existingProduct.Sku = product.Sku;
+            _dbContext.Set<Product>().Update(existingProduct);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            var existingProduct = _dbContext.Set<Product>().Find(id);
+            _dbContext.Set<Product>().Remove(existingProduct);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ CRUD1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().ToTable("Products");
+        }
+    }
+}
